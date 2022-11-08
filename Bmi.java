3b1136afa3228f5527ca9007f3971252e1ef87a7
unknown <s.satou.sys22@morijyobi.ac.jp>
2022-11-08 12:35:21 +0900

    忘れてた修正

--- Bmi.java
+++ Bmi.java
@@ -2,15 +2,13 @@ import java.util.Scanner;
 
 public class Bmi{
 	public static void main(String[] args){
-		int sinntyou,taizyuu;
-		double BMI;
 		Scanner scanner = new Scanner(System.in);
 		for(int a = 0;a < 2;a ++){
 			System.out.print("入力:");
-			sinntyou = scanner.nextInt();
+			int sinntyou = scanner.nextInt();
 			System.out.print("入力:");
-			taizyuu = scanner.nextInt();
-			BMI = taizyuu / Math.pow((double)sinntyou / 100, 2);
+			int taizyuu = scanner.nextInt();
+			Double BMI = taizyuu / Math.pow((double)sinntyou / 100, 2);
 			System.out.println((Math.floor(BMI * 100)) / 100);
 		}
 		scanner.close();
